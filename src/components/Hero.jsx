20902959dcd1338ae2aba763645c9dd036e693b8
kevin45tech <kelvinmati45@gmail.com>
2022-06-13 01:01:52 +0300

    changed the case

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,28 +1,27 @@
 import React from "react";
 
 const Hero = () => {
-    return (
-        <div
-            className="w-full h-height bg-no-repeat bg-center bg-cover  text-white bg-blend-multiply"
-            style={{
-                backgroundImage:
-                    "url(https://www.kevictours.com/wp-content/uploads/2017/06/Nairobi-national-Park-1024x640.jpg)",
-                backgroundColor: "rgba(0,0,0,0.5)",
-            }}
-        >
-            <div className=" h-full space-y-12 flex  flex-col items-start justify-center  w-mobile  md:w-3/4 mx-auto">
-                <p className="text-5xl ">
-                    Experience East African Adventure With Metrosafaris And
-                    Tours
-                </p>
-                <p>
-                    <button className=" uppercase p-3 bg-purple hover:bg-hover_color text-white rounded-full">
-                        Join the next tour
-                    </button>
-                </p>
-            </div>
-        </div>
-    );
+  return (
+    <div
+      className="w-full h-height bg-no-repeat bg-center bg-cover  text-white bg-blend-multiply"
+      style={{
+        backgroundImage:
+          "url(https://www.kevictours.com/wp-content/uploads/2017/06/Nairobi-national-Park-1024x640.jpg)",
+        backgroundColor: "rgba(0,0,0,0.5)",
+      }}
+    >
+      <div className=" h-full space-y-12 flex  flex-col items-start justify-center  w-mobile  md:w-3/4 mx-auto">
+        <p className="text-5xl uppercase ">
+          Experience East African Adventure With Metro Safaris
+        </p>
+        <p>
+          <button className=" uppercase p-3 bg-purple hover:bg-hover_color text-white rounded-full">
+            Join the next tour
+          </button>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
